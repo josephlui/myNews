83f39cb77dc8f20514f83981778eed3792bdf006
Joseph Lui <joseph.lui2000@gmail.com>
2019-02-07 00:24:24 -0500

    inital commit

--- public/assets/js/myNews.js
+++ public/assets/js/myNews.js
@@ -1,12 +1,3 @@
-// Grab the articles as a json
-$.getJSON("/articles", function(data) {
-  // For each one
-  for (var i = 0; i < data.length; i++) {
-    // Display the apropos information on the page
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
-  }
-});
-
 
 // Whenever someone clicks a p tag
 $(document).on("click", "p", function() {
@@ -22,7 +13,6 @@ $(document).on("click", "p", function() {
   })
     // With that done, add the note information to the page
     .then(function(data) {
-      console.log(data);
       // The title of the article
       $("#notes").append("<h2>" + data.title + "</h2>");
       // An input to enter a new title
@@ -31,7 +21,10 @@ $(document).on("click", "p", function() {
       $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
       // A button to submit a new note, with the id of the article saved to it
       $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-
+      if (data.note){
+        $("#notes").append("<button data-id='" + data.note._id + "' id='removenote'>Remove Note</button>");
+      }
+     
       // If there's a note in the article
       if (data.note) {
         // Place the title of the note in the title input
@@ -57,6 +50,29 @@ $(document).on("click", "#savenote", function() {
       // Value taken from note textarea
       body: $("#bodyinput").val()
     }
+  })
+    // With that done
+    .then(function(data) {
+      // Log the response
+      console.log(data);
+      // Empty the notes section
+      $("#notes").empty();
+    });
+
+  // Also, remove the values entered in the input and textarea for note entry
+  $("#titleinput").val("");
+  $("#bodyinput").val("");
+});
+
+// When you click the removenote button
+$(document).on("click", "#removenote", function() {
+  // Grab the id associated with the article from the submit button
+  var thisId = $(this).attr("data-id");
+
+  // Run a POST request to change the note, using what's entered in the inputs
+  $.ajax({
+    method: "DELETE",
+    url: "/note/" + thisId,
   })
     // With that done
     .then(function(data) {
--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
-
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
 var axios = require("axios");
 var cheerio = require("cheerio");
 
@@ -26,20 +22,50 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+// setup view engine as handlebar
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/myNews";
+
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Routes
 
 // A GET route for scraping the echoJS website
+app.get("/index", function(req, res) {
+  // check and see if there is anything in the database, if not, scrape it
+  // otherwise pull the records from the database and display data
+  db.Article.find().sort({ _id:1 })
+    .then(function(result){
+      if (result.length > 0 ){
+        // res.json(result);
+        console.log (result);
+        res.render('home',{result: result});
+      }else {
+        res.redirect("/scrape");
+      }
+    }).catch(function(err) {
+      res.render("error");
+    })
+ 
+});
+
+// // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
+
+  console.log ("scraping data");
   // First, we grab the body of the html with axios
-  axios.get("http://www.echojs.com/").then(function(response) {
+  axios.get("https://www.680news.com/").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
     // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+    $("div.post").each(function(i, element) {
       // Save an empty result object
       var result = {};
 
@@ -50,33 +76,25 @@ app.get("/scrape", function(req, res) {
       result.link = $(this)
         .children("a")
         .attr("href");
+      result.date = $(this)
+        .children("span")
+        .text();
 
       // Create a new Article using the `result` object built from scraping
       db.Article.create(result)
         .then(function(dbArticle) {
           // View the added result in the console
-          console.log(dbArticle);
+          // console.log(dbArticle);
         })
         .catch(function(err) {
           // If an error occurred, log it
           console.log(err);
+          res.render("error");
         });
     });
 
-    // Send a message to the client
-    res.send("Scrape Complete");
+    res.redirect("/index");
   });
-});
-
-// Route for getting all Articles from the db
-app.get("/articles", function(req, res) {
-  // TODO: Finish the route so it grabs all of the articles
-  db.Article.find({})
-    .then(function(result){
-      res.json(result);
-    }).catch(function(err) {
-      res.json(err);
-    })
 });
 
 // Route for grabbing a specific Article by id, populate it with it's note
@@ -90,16 +108,10 @@ app.get("/articles/:id", function(req, res) {
   }).catch(function(err){
     res.json (err);
   });
-  // TODO
-  // ====
-  // Finish the route so it finds one article using the req.params.id,
-  // and run the populate method with "note",
-  // then responds with the article with the note included
 });
 
 // Route for saving/updating an Article's associated Note
 app.post("/articles/:id", function(req, res) {
-
   const title = req.body.title;
   const body = req.body.body;
   const articleID = req.params.id;
@@ -118,13 +130,22 @@ app.post("/articles/:id", function(req, res) {
   }).catch(function(err) {
     // If an error occurred, log it
     console.log(err);
+    res.json(err);
   });
- 
-  // TODO
-  // ====
-  // save the new note that gets posted to the Notes collection
-  // then find an article from the req.params.id
-  // and update it's "note" property with the _id of the new note
+});
+
+app.delete("/note/:id", function(req, res){
+    const noteID = req.params.id;
+    console.log (noteID);
+    return db.Note.findOneAndDelete( {_id: noteID}, function(err, result) {
+      if (err){
+        console.log('error' + err);
+        res.json(err);
+      }else {
+        console.log('result' + result);
+        res.json(result);
+      }
+    })
 });
 
 // Start the server
